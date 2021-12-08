a30465df27e63ba53dc83709d1f7f0f9fc094cc7
tchiteu <matheussantos@estudante.sc.senai.br>
2021-12-08 15:11:06 -0300

    Creating map component

---
+++ src/components/map/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const Main = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #F5FCFF;
+`
+
+export const Container = styled.View`
+  width: 100%;
+  height: 100%;
+`
