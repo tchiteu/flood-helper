5c6f0f9ad93e4aad3a2c382bb9a26b00e88de4b6
tchiteu <matheussantos@estudante.sc.senai.br>
2021-12-17 20:19:06 -0300

    Adding font in global text component

--- src/components/utils/MyText/MyText.tsx
+++ src/components/utils/MyText/MyText.tsx
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import { CustomText } from './style';
 
 interface MyProps {
-  size?: Number 
+  size?: Number,
+  color?: string
 }
 
 export class MyText extends Component<MyProps> {
 
   render() {
     return (
-      <CustomText size={this.props.size} style={{fontFamily: 'Roboto'}}>
+      <CustomText
+        size={this.props.size}
+        style={{fontFamily: 'Roboto'}}
+        color={this.props.color}
+      >
         {this.props.children}
       </CustomText>
     )
